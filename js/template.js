f79bc56aaa18e7f38df44b4ec7946f8216e55b01
metalfinger <hir.012612@gmail.com>
2018-12-20 09:18:26 +0530

    still image test

--- js/template.js
+++ js/template.js
@@ -59,7 +59,7 @@
 				counter_string = "0"+counter_string;
 			}
 
-			var nextImage =	"img_seq/sphere-wave_000"+counter_string+".png"
+			// var nextImage =	"img_seq/sphere-wave_000"+counter_string+".png"
 
 			$(".myImageHolder").attr('src', nextImage);
 
